89e1ce3f9b9fed03baf5d9acafc8276f5b6fece3
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-02 09:05:10 -0300

    feat: configurate socket events

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application } from "express";
 import http from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 export class App {
   private app: Application;
@@ -14,8 +14,26 @@ export class App {
   }
 
   public listen() {
-    this.app.listen(3333, () =>
+    this.http.listen(3333, () =>
       console.log("Server is runing on port 3333 🔥"),
     );
+  }
+
+  public listenSocket() {
+    this.io.of("/meeting").on("connection", this.socketEvents);
+  }
+
+  private socketEvents(socket: Socket) {
+    socket.on("subscribe", data => {
+      socket.join(data.meetingId);
+
+      socket.on("chat", data =>
+        socket.broadcast.to(data.meetingId).emit("chat", {
+          text: data.text,
+          username: data.username,
+          time: data.time,
+        }),
+      );
+    });
   }
 }
